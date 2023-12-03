5407fd9e537aa3e95ec5fa9ddbdd6d2a87ed6ec7
Khalil Khan <khalilahmad.me@gmail.com>
2023-12-03 15:04:23 +0100

    accessing user location through geolocation and if error happen then access through IP

--- src/components/UserLocation.jsx
+++ src/components/UserLocation.jsx
@@ -1,44 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 function UserLocation() {
+    const [location, setLocation] = useState({ latitude: "", longitude: "" });
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const [location, setLocaton] = useState(null)
+  useEffect(() => {
 
-    const handleLocationClick = () => {
-        if(navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    
-                    setLocaton({
-                        latitude: position.coords.latitude,
-                        longitude: position.coords.longitude,
-                    })
-                },
-                (error) => {
-                    console.error("Error getting location:", error.message);
-                }
-            )
-        } else {
-            console.error("Geolocation is not supported by your browser")
-        }
-    }
-    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLocation({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                });
+                setLoading(false)
+            },
+            (Error) => {
+                // If there is an error or user denies permission, fetch location using IP
+                fetchByIP();
+              }
+        )
+    } else {
+        fetchByIP()
+    } 
+   
+
+  }, [])
 
 
-    return (
-        <div>
-      <h2>User Location</h2>
-      {location ? (
-        <div>
-          <p>Latitude: {location.latitude}</p>
-          <p>Longitude: {location.longitude}</p>
-        </div>
+const fetchByIP = () => {
+    fetch("https://geolocation-db.com/json/")
+      .then((response) => response.json())
+      .then((data) => {
+        setLocation({
+          latitude: data.latitude,
+          longitude: data.longitude,
+        });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError("Error fetching geolocation data");
+        console.error("Error fetching geolocation data:", error);
+        setLoading(false);
+      });
+}
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
       ) : (
-        <button onClick={handleLocationClick}>Get Location</button>
+        <div>
+          <p>Your latitude is: {location.latitude}</p>
+          <p>Your longitude is: {location.longitude}</p>
+        </div>
       )}
     </div>
-    )
+  );
 }
 
 export default UserLocation;
